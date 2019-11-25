538a8a1694b21ae77e9b18d3716195ced82cf5ab
Hayden McFarland <mcfarlandsms@gmail.com>
2019-11-24 21:34:33 -0800

    formatting

--- app/javascript/app.vue
+++ app/javascript/app.vue
@@ -1,52 +1,52 @@
 <template>
-    <v-app id="app" :dark="setTheme">
-        <v-app-bar app>
-            <v-toolbar-title>
-                <router-link to="/">
-                    <v-btn icon>
-                        <v-icon>mdi-home</v-icon>
+<v-app id="app" :dark="setTheme">
+    <v-app-bar app>
+        <v-toolbar-title>
+            <router-link to="/">
+                <v-btn icon>
+                    <v-icon>mdi-home</v-icon>
+                </v-btn>
+            </router-link>
+        </v-toolbar-title>
+        <v-spacer></v-spacer>
+        <div v-if="!signedIn()">
+            <router-link to="/signin">
+                <v-btn icon>
+                    <v-icon>mdi-account-arrow-right</v-icon>
+                </v-btn>
+            </router-link>
+        </div>
+        <div v-else>
+            <v-menu offset-y>
+                <template v-slot:activator="{ on }">
+                    <v-btn v-on="on">
+                        User
                     </v-btn>
-                </router-link>
-            </v-toolbar-title>
-            <v-spacer></v-spacer>
-                <div v-if="!signedIn()">
-                    <router-link to="/signin">
-                        <v-btn icon>
-                            <v-icon>mdi-account-arrow-right</v-icon>
-                        </v-btn>
-                    </router-link>
-                </div>
-                <div v-else>
-                    <v-menu offset-y>
-                        <template v-slot:activator="{ on }">
-                        <v-btn v-on="on">
-                            User
-                        </v-btn>
-                        </template>
-                        <v-list>
-                            <v-list-item>
-                                <v-switch v-model="goDark"></v-switch>
-                                    <a href="#" @click.prevent="signOut">
-                                <v-btn icon>
-                                    <v-icon>mdi-account-off</v-icon>
-                                </v-btn>
-                                </a>
-                            </v-list-item>
-                        </v-list>
-                    </v-menu>
-                </div>
-        </v-app-bar>
-        <v-content>
-            <v-container fluid>
-                <v-row justify="center">
-                    <router-view/>
-                </v-row>
-            </v-container>
-        </v-content>
-        <v-bottom-navigation fixed>
-            <img src="~/images/logo.svg"/>
-        </v-bottom-navigation>
-    </v-app>
+                </template>
+                <v-list>
+                    <v-list-item>
+                        <v-switch v-model="goDark"></v-switch>
+                        <a href="#" @click.prevent="signOut">
+                            <v-btn icon>
+                                <v-icon>mdi-account-off</v-icon>
+                            </v-btn>
+                        </a>
+                    </v-list-item>
+                </v-list>
+            </v-menu>
+        </div>
+    </v-app-bar>
+    <v-content>
+        <v-container fluid>
+            <v-row justify="center">
+                <router-view />
+            </v-row>
+        </v-container>
+    </v-content>
+    <v-bottom-navigation fixed>
+        <img src="~/images/logo.svg" />
+    </v-bottom-navigation>
+</v-app>
 </template>
 
 <script>
@@ -57,11 +57,11 @@ Vue.use(Vuetify)
 import 'images/logo.svg'
 
 export default {
-  created() {
-    this.$vuetify.theme.dark = false;
-    this.signedIn();
-  },
-  data() {
+    created() {
+        this.$vuetify.theme.dark = false;
+        this.signedIn();
+    },
+    data() {
         return {
             goDark: false
         };
@@ -76,20 +76,20 @@ export default {
         }
     },
     methods: {
-        setError (error, text) {
-        this.error = (error.response && error.response.data && error.response.data.error) || text
+        setError(error, text) {
+            this.error = (error.response && error.response.data && error.response.data.error) || text
         },
-        signedIn () {
-        return localStorage.signedIn
+        signedIn() {
+            return localStorage.signedIn
         },
-        signOut () {
-        this.$http.secured.delete('/signin')
-            .then(response => {
-            delete localStorage.csrf
-            delete localStorage.signedIn
-            this.$router.replace('/')
-            })
-            .catch(error => this.setError(error, 'Cannot sign out'))
+        signOut() {
+            this.$http.secured.delete('/signin')
+                .then(response => {
+                    delete localStorage.csrf
+                    delete localStorage.signedIn
+                    this.$router.replace('/')
+                })
+                .catch(error => this.setError(error, 'Cannot sign out'))
         }
     }
 }
@@ -97,9 +97,11 @@ export default {
 
 <style scoped>
 p {
-  font-size: 2em;
-  text-align: center;
+    font-size: 2em;
+    text-align: center;
 }
 
-a {  text-decoration: none; }
+a {
+    text-decoration: none;
+}
 </style>
