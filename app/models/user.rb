676596e4585f6e82773ce561ed23741f45178d44
Hayden McFarland <mcfarlandsms@gmail.com>
2019-11-28 00:08:49 -0800

    more

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,8 +4,6 @@ class User < ApplicationRecord
   include Devise::JWT::RevocationStrategies::JTIMatcher
   include Tokenizable
 
-  has_secure_password
-
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable,
@@ -20,20 +18,17 @@ class User < ApplicationRecord
   # add new roles to the end
   enum role: %i[customer admin]
 
-  # - RELATIONS
-  # -
-
   # - VALIDATIONS
   validates :email, presence: true, uniqueness: true
   validates :email, length: { maximum: 255 }
-  validates :email, format: { with: Regex::Email::VALIDATE }
+  validates :email, format: { with: Devise.email_regexp }
   validates :first_name, presence: true
   validates :first_name, length: { maximum: 255 }
   validates :last_name, presence: true
   validates :last_name, length: { maximum: 255 }
 
   # - CALLBACKS
-  after_initialize :setup_new_user, if: :new_record?
+  # after_initialize :setup_new_user, if: :new_record?
 
   # Send mail through activejob
   def send_devise_notification(notification, *args)
@@ -45,7 +40,7 @@ class User < ApplicationRecord
     [first_name, last_name].join(' ').strip
   end
 
-  private def setup_new_user
-    self.role ||= :customer
-  end
+  #private def setup_new_user
+  #  #
+  #end
 end
