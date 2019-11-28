db4a36cb90a1ce7e52e6660d8101074303d91acc
Hayden McFarland <mcfarlandsms@gmail.com>
2019-11-28 14:54:33 -0800

    clean up routes

--- config/routes.rb
+++ config/routes.rb
@@ -5,20 +5,10 @@ Rails.application.routes.draw do
     mount GraphiQL::Rails::Engine, at: '/graphiql', graphql_path: '/graphql'
   end
   post '/graphql', to: 'graphql#execute'
+
   root 'landing#index'
   get 'landing/index'
 
-  namespace :api do
-    namespace :v1 do
-      # routes go here
-    end
-  end
-
-  post 'refresh', controller: :refresh, action: :create
-  post 'signin', controller: :signin, action: :create
-  post 'signup', controller: :signup, action: :create
-  delete 'signin', controller: :signin, action: :destroy
-
   # utilized to use vue-router history mode
-  match /^(graphql)/, to: redirect('/?redirect=%{path}'), via: :all
+  match '*path', to: redirect('/?redirect=%{path}'), via: :all
 end
