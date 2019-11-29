d21f7b23a90e855f0a575c50dfb980f732debda2
Hayden McFarland <mcfarlandsms@gmail.com>
2019-11-28 21:02:02 -0800

    fix import

--- app/javascript/packs/plugins/router.js
+++ app/javascript/packs/plugins/router.js
@@ -1,7 +1,7 @@
 import Vue from "vue";
 import Router from "vue-router";
-import Signin from "../components/sign_in.vue";
-import Signup from "../components/sign_up.vue";
+import Signin from "../components/auth/sign_in.vue";
+import Signup from "../components/auth/sign_up.vue";
 Vue.use(Router);
 
 const router = new Router({
