ec7625ff979fed36ce589b8f3b0fdebad60e8673
Hayden McFarland <mcfarlandsms@gmail.com>
2019-12-03 22:07:36 -0800

    rubocop

--- app/graphql/mutations/auth/logout.rb
+++ app/graphql/mutations/auth/logout.rb
@@ -8,7 +8,7 @@ module Mutations
       description 'Logout for users'
 
       field :result, GraphQL::Types::Boolean, null: false
-      #type GraphQL::Types::Boolean
+      # type GraphQL::Types::Boolean
 
       def grille_resolver
         if context[:current_user]
