7f6bc953eb6bfca54d59a7fe3b6075b5ad223b62
Hayden McFarland <mcfarlandsms@gmail.com>
2019-12-05 20:22:56 -0800

    enhance base mutation resolver

--- app/graphql/mutations/base_mutation.rb
+++ app/graphql/mutations/base_mutation.rb
@@ -1,38 +1,41 @@
 # frozen_string_literal: true
 
-class Mutations::BaseMutation < GraphQL::Schema::Mutation
-  null false
+module Mutations
+  class BaseMutation < GraphQL::Schema::Mutation
+    null false
 
-  field :success, GraphQL::Types::Boolean, null: false
-  field :errors, [GraphQL::Types::String], null: false
+    field :success, GraphQL::Types::Boolean, null: false
+    field :errors, [GraphQL::Types::String], null: false
 
-  def grille_resolver(*_args)
-    raise "\'#{__method__}' must be declared in mutation"
-  end
+    def grille_resolver(*_args)
+      raise "\'#{__method__}' must be declared in mutation"
+    end
 
-  def resolve(*args)
-    result = grille_resolver(*args)
+    def wrap_result(result)
+      if result.is_a?(ActiveRecord::Base)
+        return {
+          obj: { result.class.name.demodulize.downcase => result },
+          success: result.persisted?,
+          errors: result.error
+        }
+      end
 
-    if result.is_a?(ActiveRecord::Base)
-      Mutations::MutationResult.call(
-        obj: { result.class.name.demodulize.downcase => result },
-        success: result.persisted?,
-        errors: result.errors
-      )
-    elsif [true, false].include?(result)
-      Mutations::MutationResult.call(
-        obj: { result: result },
-        success: result,
-        errors: []
-      )
-    elsif result.nil?
-      Mutations::MutationResult.call(
-        obj: { user: Types::Auth::UserType.empty },
-        success: false,
-        errors: []
-      )
-    else
+      if [true, false].include?(result)
+        return { obj: { result: result }, success: result, errors: [] }
+      end
+
+      if result.nil?
+        null_obj_keys = self.class.fields.keys - %w[success errors]
+        null_obj = null_obj_keys.each_with_object({}) { |k, h| h[k] = {} }
+        return { obj: null_obj, success: false, errors: [] }
+      end
+
       raise "unsupported resolver type: '#{result.class}'"
+    end
+
+    def resolve(*args)
+      result = grille_resolver(*args)
+      Mutations::MutationResult.call(**wrap_result(result))
     end
   end
 end
--- app/graphql/types/auth/user_type.rb
+++ app/graphql/types/auth/user_type.rb
@@ -3,24 +3,14 @@
 module Types
   module Auth
     class UserType < BaseObject
-      field :id, ID, null: false
-      field :name, String, null: false
-      field :firstName, String, null: false
-      field :lastName, String, null: false
+      field :id, ID, null: true
+      field :name, String, null: true
+      field :firstName, String, null: true
+      field :lastName, String, null: true
       field :email, String, null: true
-      field :token, String, null: false
+      field :token, String, null: true
 
       class << self
-        def empty
-          OpenStruct.new(
-            id: '',
-            name: '',
-            firstName: '',
-            lastName: '',
-            email: '',
-            token: ''
-          )
-        end
       end
     end
   end
