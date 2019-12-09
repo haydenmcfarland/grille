e08becf0c87fc596881a32a07ed15db3e7bc14de
Hayden McFarland <mcfarlandsms@gmail.com>
2019-12-08 22:55:52 -0800

    shrink query resolver

--- app/graphql/queries/base_query.rb
+++ app/graphql/queries/base_query.rb
@@ -7,35 +7,8 @@ module Queries
       raise "'#{__method__}' must be declared in mutation"
     end
 
-    def wrap_result(result)
-      if result.is_a?(ActiveRecord::Relation)
-        return result
-      end
-
-      if result.is_a?(ActiveRecord::Base)
-        return {
-          result.class.name.demodulize.downcase => result,
-          success: result.persisted?,
-          errors: result.errors
-        }
-      end
-
-      if [true, false].include?(result)
-        return { result: result, success: true, errors: [] }
-      end
-
-      if result.nil?
-        null_obj_keys = self.class.fields.keys - %w[success errors]
-        null_obj = null_obj_keys.each_with_object({}) { |k, h| h[k] = {} }
-        return null_obj.merge(success: false, errors: [])
-      end
-
-      raise "unsupported resolver type: '#{result.class}'"
-    end
-
     def resolve(*args)
-      result = grille_resolver(*args)
-      wrap_result(result)
+      return grille_resolver(*args)
     end
   end
 end
