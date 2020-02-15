f47d9a70948d8a3e790c0bd67c5f21cb2488daa6
Hayden McFarland <mcfarlandsms@gmail.com>
2020-02-14 18:45:26 -0800

    update dummy application

--- spec/dummy/config/webpack/environment.js
+++ spec/dummy/config/webpack/environment.js
@@ -1,3 +1,17 @@
 const { environment } = require('@rails/webpacker')
+const erb = require('./loaders/erb')
+const { VueLoaderPlugin } = require('vue-loader')
+const vue = require('./loaders/vue')
 
+environment.plugins.prepend('VueLoaderPlugin', new VueLoaderPlugin())
+environment.loaders.prepend('vue', vue)
+
+const sassLoader = environment.loaders.get('sass')
+const sassLoaderConfig = sassLoader.use.find(function(element) {
+    return element.loader == 'sass-loader'
+})
+
+const options = sassLoaderConfig.options
+options.implementation = require('sass')
+environment.loaders.prepend('erb', erb)
 module.exports = environment
