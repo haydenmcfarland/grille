b1b1a5f7c72590948fca802dd59d0c4dce796755
Hayden McFarland <mcfarlandsms@gmail.com>
2020-03-28 13:45:19 -0700

    refactor configurable

--- lib/grille/configurable.rb
+++ lib/grille/configurable.rb
@@ -2,16 +2,17 @@
 
 module Grille
   module Configurable
-    # should return a hash or an object that supports to_h
-    def default_config; end
-
+    # overwrite to call component initialization
+    def build; end
+    
     def configure
       config = default_config
       yield(config)
       config.to_h.each { |k, v| send("#{k}=", v) }
       build
     end
-
-    def build; end
+    
+    # should return a hash or an object that supports to_h
+    def default_config; end
   end
 end
