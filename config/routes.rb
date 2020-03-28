4460c77b6b44f1da3c60699f7868d051a058c7c4
Hayden McFarland <mcfarlandsms@gmail.com>
2020-03-28 16:57:44 -0700

    rubocop and refactor

--- config/routes.rb
+++ config/routes.rb
@@ -7,12 +7,10 @@ Grille::Engine.routes.draw do
   get 'landing/index'
 
   path_constraints = {}
-  if Rails.env.development?
-    if ENV['GRILLE_GRAPHI_QL'] == 'true'
-      require 'graphiql/rails'
-      mount GraphiQL::Rails::Engine, at: '/graphiql', graphql_path: '/graphql'
-      path_constraints[:path] = /((?!graphiql).)*/
-    end
+  if Rails.env.development? && ENV['GRILLE_GRAPHI_QL'] == 'true'
+    require 'graphiql/rails'
+    mount GraphiQL::Rails::Engine, at: '/graphiql', graphql_path: '/graphql'
+    path_constraints[:path] = /((?!graphiql).)*/
   end
 
   # utilized to use vue-router history mode
--- lib/grille/configurable.rb
+++ lib/grille/configurable.rb
@@ -4,14 +4,14 @@ module Grille
   module Configurable
     # overwrite to call component initialization
     def build; end
-    
+
     def configure
       config = default_config
       yield(config)
       config.to_h.each { |k, v| send("#{k}=", v) }
       build
     end
-    
+
     # should return a hash or an object that supports to_h
     def default_config; end
   end
