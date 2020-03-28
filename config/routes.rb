cfb5027687a7c95f106284686603963fe5027b17
Hayden McFarland <mcfarlandsms@gmail.com>
2020-03-28 16:55:31 -0700

    only constrain path resolution if graphiql is enabled for development

--- config/routes.rb
+++ config/routes.rb
@@ -6,15 +6,17 @@ Grille::Engine.routes.draw do
   root 'landing#index'
   get 'landing/index'
 
-  # utilized to use vue-router history mode
-  get ':path', to: redirect('/?redirect=%{path}'), constraints: {
-    path: /((?!graphiql).)*/
-  }
-
-  if ENV['GRILLE_GRAPHI_QL'] == 'true' && Rails.env.development?
-    require 'graphiql/rails'
-    mount GraphiQL::Rails::Engine, at: '/graphiql', graphql_path: '/graphql'
+  path_constraints = {}
+  if Rails.env.development?
+    if ENV['GRILLE_GRAPHI_QL'] == 'true'
+      require 'graphiql/rails'
+      mount GraphiQL::Rails::Engine, at: '/graphiql', graphql_path: '/graphql'
+      path_constraints[:path] = /((?!graphiql).)*/
+    end
   end
+
+  # utilized to use vue-router history mode
+  get ':path', to: redirect('/?redirect=%{path}'), constraints: path_constraints
 
   devise_for :users, class_name: 'Grille::User', module: :devise
 end
