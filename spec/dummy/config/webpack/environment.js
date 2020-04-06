39f75ca980ab8cdb4495ce0a0083ab4a4a97f473
Hayden McFarland <mcfarlandsms@gmail.com>
2020-04-05 19:50:26 -0700

    oh yesss

--- spec/dummy/config/webpack/environment.js
+++ spec/dummy/config/webpack/environment.js
@@ -5,6 +5,7 @@ const vue = require('./loaders/vue')
 
 environment.plugins.prepend('VueLoaderPlugin', new VueLoaderPlugin())
 environment.loaders.prepend('vue', vue)
+environment.config.resolve.alias = { 'vue$': 'vue/dist/vue.esm.js' }; // <- add alias
 
 const sassLoader = environment.loaders.get('sass')
 const sassLoaderConfig = sassLoader.use.find(function(element) {
