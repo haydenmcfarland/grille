444f92e74610214d8310ab12864c578942122088
Hayden McFarland <mcfarlandsms@gmail.com>
2020-04-16 19:06:12 -0700

    more refactor

--- app/services/grille/component_importer.rb
+++ app/services/grille/component_importer.rb
@@ -5,7 +5,10 @@ require_relative '../../../lib/vue/inline_template_component'
 
 module Grille
   module ComponentImporter
-    mattr_accessor :component_names, :mixin_names
+    mattr_accessor :component_names, default: {}
+    mattr_accessor :mixin_names, default: {}
+    mattr_accessor :imports, default: Set.new
+
     COMPONENTS_PATH = Pathname.new(
       File.join(__dir__, '../../components/grille/components')
     )
@@ -18,34 +21,29 @@ module Grille
     module_function
 
     def component_name(klass)
-      component_names ||= {}
       component_names[klass] ||= klass.name.gsub('::', '').camelize
     end
 
     def mixin_name(klass)
-      mixin_names ||= {}
       mixin_names[klass] ||= component_name(klass) + 'Mixin' if klass.mixins
     end
 
     def call
-      imports = Set.new
       Grille::Components::Base.descendants.map do |klass|
         grille_component = klass.new
         vue_component = ::Vue::SingleFileComponent.new(grille_component.render)
 
-        new_imports = (vue_component.imports - imports.to_a).join("\n")
-        imports += vue_component.imports
+        new_imports = imports.difference(vue_component.imports).to_a.join("\n")
+        imports.add(vue_component.imports)
 
         mixins = klass.grille_ancestors.map { |a| mixin_name(a) }.compact
         if mixins.present?
           mixin_imports = klass.grille_ancestors.map do |k|
-            <<-JS
-                import #{mixin_name(k)} from '#{k.mixins_path}';
-            JS
+            "import #{mixin_name(k)} from '#{k.mixins_path}';"
           end
 
-          new_mixin_imports = (mixin_imports - imports.to_a).join("\n")
-          imports += mixin_imports
+          new_mixin_imports = imports.difference(mixin_imports).to_a.join("\n")
+          imports.add(mixin_imports)
         end
 
         # FIXME: need to support styles as well in single file components
